76b9475b0e94a685a313e1c5ae2ce020ee9628be
stayinHazey <alessiocats@gmail.com>
2022-11-01 16:10:56 -0400

    The Move and Jump update

--- Assets/Plaformer/MovementPlatformer.cs
+++ Assets/Plaformer/MovementPlatformer.cs
@@ -6,16 +6,34 @@ public class MovementPlatformer : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rb;
-    Vector2 movement;
+    float xMove;
+    bool jumpPress;
 
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
+        //Checking for horizontal keyboard input
+        xMove = Input.GetAxisRaw("Horizontal");
+
+        //Checking for spacebar keyboard input
+        if(Input.GetKey(KeyCode.Space))
+        {
+            jumpPress = true;
+        }
+        else
+        {
+            jumpPress = false;
+        }
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime * moveSpeed);
+        // Moves us left and right
+        rb.velocity = new Vector2(xMove * moveSpeed, rb.velocity.y);
+
+        // Jump
+        if(jumpPress){
+            rb.velocity = new Vector2(rb.velocity.x, moveSpeed);
+        }
     }
 }
