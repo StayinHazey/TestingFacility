8e615889caf5d978dd9e73834446c7409900a1d1
stayinHazey <alessiocats@gmail.com>
2022-11-02 01:48:17 -0400

    The Fixed Jump Update

--- Assets/Plaformer/MovementPlatformer.cs
+++ Assets/Plaformer/MovementPlatformer.cs
@@ -8,6 +8,8 @@ public class MovementPlatformer : MonoBehaviour
     public Rigidbody2D rb;
     float xMove;
     bool jumpPress;
+    public Animator anim;
+    private bool grounded;
 
     // Update is called once per frame
     void Update()
@@ -16,7 +18,7 @@ public class MovementPlatformer : MonoBehaviour
         xMove = Input.GetAxisRaw("Horizontal");
 
         //Checking for spacebar keyboard input
-        if(Input.GetKey(KeyCode.Space))
+        if(Input.GetKey(KeyCode.Space) && grounded)
         {
             jumpPress = true;
         }
@@ -24,6 +26,28 @@ public class MovementPlatformer : MonoBehaviour
         {
             jumpPress = false;
         }
+        
+
+
+        //Flipping the sprite for left and right facing
+        //Right Facing
+        if(xMove > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        //Left Facing
+        else if(xMove < -0.01f)
+        {
+            transform.localScale = new Vector3(-1,1,1);
+        }
+
+        //Run Animation Transition to and from Idle
+        //If its -1 its left moving, if its 1 its right moving... If its 0 its idle. So
+        //It reads this to determine the bool; if its not 0, run is true.
+        anim.SetBool("run", xMove != 0);
+
+        //Animation for jumping
+        anim.SetBool("grounded", grounded);
     }
 
     void FixedUpdate()
@@ -33,7 +57,23 @@ public class MovementPlatformer : MonoBehaviour
 
         // Jump
         if(jumpPress){
-            rb.velocity = new Vector2(rb.velocity.x, moveSpeed);
+            Jump();
         }
     }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, moveSpeed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+
 }
